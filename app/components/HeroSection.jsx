73c76eba714c06b93a27b7ee4c4944233f033b72
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-23 17:09:43 +0100

    GSAP Hero Section

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+export const HeroSection = () => {
+    return (
+        <>
+            <div className="min-h-screen bg-[#282828] text-white flex flex-col items-center relative">
+                <h1 className="text-[8.5rem] font-extralight tracking-tight absolute top-[10rem]">
+                    WONDERKIN TATTOO
+                </h1>
+                <img src="leave.png" className="h-[300] w-[300] absolute bottom-0" alt="john" />
+                <div className="w-[586px] h-[674px] bg-gray-500 rounded-t-full"></div>
+            </div>
+        </>
+    )
+}
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { HeroSection } from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+
+    </>
+  );
+}
