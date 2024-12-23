d6718864403be725288e9ccd141740a87a4aba96
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-23 17:36:28 +0100

    Hero Section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -5,9 +5,10 @@ export const HeroSection = () => {
                 <h1 className="text-[8.5rem] font-extralight tracking-tight absolute top-[10rem]">
                     WONDERKIN TATTOO
                 </h1>
-                <img src="leave.png" className="h-[300] w-[300] absolute bottom-0" alt="john" />
-                <div className="w-[586px] h-[674px] bg-gray-500 rounded-t-full"></div>
+                <div className="w-[586px] h-[674px] bg-gray-500 rounded-t-full absolute z-10"></div>
+                <img src="leave.png" className="h-[300] w-[700] absolute bottom-0" alt="john" />
+                <img src="john.jpg" className="w-[300px] h-[400px] z-10 bottom-0 absolute" alt="tattoed girl" />
             </div>
         </>
     )
-}
+} 
